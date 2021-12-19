7200fe258c66443ea2fa305c84dddcd605d7117f
kotand <x3ther@ya.ru>
2021-12-19 21:08:39 +0700

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class Info {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("19.01.2020");
+        System.out.println("Hello world");
     }
 }
---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Andrey Kudrenko");
+        System.out.println("10.05.1997");
+    }
+}
