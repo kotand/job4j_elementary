e130c799bdafc3b70f63318904c5d44c7e5544d9
kotand <x3ther@ya.ru>
2021-12-22 21:21:24 +0700

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -9,7 +9,6 @@ public class ConverterTest {
     public void whenConvert420RblThen6Euro() {
         float euro = Converter.rubleToEuro(420);
         float expectedEuro = 6;
-        boolean passedEuro = expectedEuro == euro;
         float eps = 0.0001f;
         Assert.assertEquals(expectedEuro, euro, eps);
     }
@@ -18,7 +17,6 @@ public class ConverterTest {
     public void whenConvert420RblThen7Dollar() {
         float dollar = Converter.rubleToDollar(420);
         float expectedDollar = 7;
-        boolean passedDollar = expectedDollar == dollar;
         float eps = 0.0001f;
         Assert.assertEquals(expectedDollar, dollar, eps);
     }
