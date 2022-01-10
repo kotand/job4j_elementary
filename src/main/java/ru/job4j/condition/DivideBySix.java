47edfb8de799755d09362619141ca79963a7d77b
kotand <x3ther@ya.ru>
2022-01-10 12:00:37 +0700

    4.6.2. Switch - matcher [#334071]

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -3,11 +3,11 @@ package ru.job4j.condition;
 public class DivideBySix {
     public static String checkNumber(int number) {
         String rsl;
-        if (number % 6 == 0 && number % 3 == 0 &&  number % 2 == 0) {
+        if (number % 3 == 0 &&  number % 2 == 0) {
             rsl = "Исходное число делится на 6.";
-        } else if (number % 3 == 0 && number % 2 == 1) {
+        } else if (number % 3 == 0) {
             rsl = "Исходное число делится на 3, но не является четным.";
-        } else if (number % 3 >= 1 && number % 2 == 0) {
+        } else if (number % 2 == 0) {
             rsl = "Исходное число не делится на 3, но является четным.";
         } else {
             rsl = "Исходное число не делится на 3 и не является четным.";
