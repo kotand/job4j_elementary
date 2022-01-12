6a962fb41401f9ea99b06bdf64d57165134dfbc3
kotand <x3ther@ya.ru>
2022-01-12 14:19:53 +0700

    5.8. Именование классов и переменных [#4087 #249820]

---
+++ src/main/java/ru/job4j/loop/CheckstyleCommit.java
@@ -0,0 +1,8 @@
+package ru.job4j.loop;
+
+public class CheckstyleCommit {
+    public static void main(String[] args) {
+        String name = "Andrew";
+        System.out.println(name + " Checkstyle");
+    }
+}
