be87fa7e61060c1d2057bafac342d2fe291ca482
kotand <x3ther@ya.ru>
2022-02-03 17:04:23 +0700

    6.0.1. Объявление массива. [#3613 #250253]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,14 +2,8 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        String[] names = new String[4];
-        names[0] = "Andrew";
-        names[1] = "Alex";
-        names[2] = "Sergey";
-        names[3] = "Maksim";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
     }
 }
